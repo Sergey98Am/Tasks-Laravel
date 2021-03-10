4f4d8a712ed6cbc9e7737c100c0604eeb4ebc80e
Sergey98Am <serg98barca@gmail.com>
2021-03-10 15:00:47 +0400

    added list CRUD

---
+++ app/Models/Lists.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Lists extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'board_id',
+    ];
+
+    public function board()
+    {
+        return $this->belongsTo('App\Models\Board');
+    }
+}
